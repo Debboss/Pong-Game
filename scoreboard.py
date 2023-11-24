745e89c37704c6bf6e9146d31c8749ca75347da9
Debbos <117668180+Debboss@users.noreply.github.com>
2023-11-24 09:49:18 +0200

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_score()
+
+    def update_scoreboard(self):
+        self.clear()  # Clear previous score
+        self.goto(-100, 200)
+        self.write(f"{self.l_score} | {self.r_score}", align="center", font=("Courier", 24, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
